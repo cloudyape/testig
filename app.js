a89c6276db498d4749c012fe6df030974963ffcb
unknown <ankitjobalerts@gmail.com>
2022-04-08 00:27:44 +0530

    Adding index

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var routes = require("./routes/routes.js")(app);
-var port = process.env.port || 8080;
+var port = process.env.port || 3000;
 var server = app.listen(port, function () {
     console.log("Listening on port %s...", server.address().port);
 });
