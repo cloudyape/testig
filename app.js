8c155bf28cd85b301f13a3ffc67fb0123c9d4c13
unknown <ankitjobalerts@gmail.com>
2022-04-08 01:45:58 +0530

    Adding index

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-var routes = require("./routes/routes.js")(app);
-var port = process.env.port || 3000;
-var server = app.listen(port, function () {
-    console.log("Listening on port %s...", server.address().port);
-});
+const express = require('express');
+const app = express();
+var port = process.env.PORT || 3000;
+app.get('/' , (req, res) => res.send('Hello World'));
+app.listen(port, () => console.log('Server is runnig on port' + port));
