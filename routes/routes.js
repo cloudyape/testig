810220ec48d0b5f51997cb254f3357e93c889a33
unknown <ankitjobalerts@gmail.com>
2022-04-07 18:56:26 +0530

    testing commit

---
+++ routes/routes.js
@@ -0,0 +1,22 @@
+var appRouter = function (app) {
+    app.get("/", function (req, res) {
+        res.send("Hello World");
+    });
+
+    app.get("/account", function (req, res) {
+        var accountMock = {
+            "username": "nraboy",
+            "password": "1234",
+            "twitter": "@nraboy"
+        }
+        if (!req.query.username) {
+            return res.send({ "status": "error", "message": "missing username" });
+        } else if (req.query.username != accountMock.username) {
+            return res.send({ "status": "error", "message": "wrong username" });
+        } else {
+            return res.send(accountMock);
+        }
+    });
+}
+
+module.exports = appRouter;
